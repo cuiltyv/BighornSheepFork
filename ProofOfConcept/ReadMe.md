ce9113dbdf7b37de701eab92b78e50611ea1c97b
Alberto Tamez <69254699+alberto-tamez@users.noreply.github.com>
2024-03-10 18:00:47 -0600

    First version python twilio connection

---
+++ ProofOfConcept/ReadMe.md
@@ -0,0 +1,11 @@
+# Instructions
+
+Instala los requirements con 
+
+```bash
+pip install -r requirements.txt
+```
+
+Yo estoy siguiendo las instruciones de este sitio web
+https://www.twilio.com/en-us/blog/transcribe-phone-calls-text-real-time-twilio-vosk
+
---
+++ ProofOfConcept/app.py
@@ -0,0 +1,76 @@
+# Loading the env variables
+# from dotenv import load_dotenv
+# load_dotenv()  # This loads the environment variables from the .env file
+
+# import os
+
+# # Now you can access your Twilio credentials safely
+# TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+# TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+# End of loading .env variables
+
+## Tutorial code
+import audioop
+import base64
+import json
+import os
+from flask import Flask, request
+from flask_sock import Sock, ConnectionClosed
+from twilio.twiml.voice_response import VoiceResponse, Start
+from twilio.rest import Client
+import vosk
+
+app = Flask(__name__)
+sock = Sock(app)
+twilio_client = Client()
+model = vosk.Model('model')
+
+CL = '\x1b[0K'
+BS = '\x08'
+
+
+@app.route('/call', methods=['POST'])
+def call():
+    """Accept a phone call."""
+    response = VoiceResponse()
+    start = Start()
+    start.stream(url=f'wss://{request.host}/stream')
+    response.append(start)
+    response.say('Please leave a message')
+    response.pause(length=60)
+    print(f'Incoming call from {request.form["From"]}')
+    return str(response), 200, {'Content-Type': 'text/xml'}
+
+
+@sock.route('/stream')
+def stream(ws):
+    """Receive and transcribe audio stream."""
+    rec = vosk.KaldiRecognizer(model, 16000)
+    while True:
+        message = ws.receive()
+        packet = json.loads(message)
+        if packet['event'] == 'start':
+            print('Streaming is starting')
+        elif packet['event'] == 'stop':
+            print('\nStreaming has stopped')
+        elif packet['event'] == 'media':
+            audio = base64.b64decode(packet['media']['payload'])
+            audio = audioop.ulaw2lin(audio, 2)
+            audio = audioop.ratecv(audio, 2, 1, 8000, 16000, None)[0]
+            if rec.AcceptWaveform(audio):
+                r = json.loads(rec.Result())
+                print(CL + r['text'] + ' ', end='', flush=True)
+            else:
+                r = json.loads(rec.PartialResult())
+                print(CL + r['partial'] + BS * len(r['partial']))
+
+
+if __name__ == '__main__':
+    from pyngrok import ngrok
+    port = 8080
+    public_url = ngrok.connect(port, bind_tls=True).public_url
+    number = twilio_client.incoming_phone_numbers.list()[0]
+    number.update(voice_url=public_url + '/call')
+    print(f'Waiting for calls on {number.phone_number}')
+
+    app.run(port=port)
