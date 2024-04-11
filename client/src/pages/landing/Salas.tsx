d4af4c333ba5c0363d0dcda3dcb1efcab55de3de
RobAndonie <a01411863@tec.mx>
2024-04-11 11:47:40 -0600

    I have to update to switch branches

--- client/src/pages/landing/Salas.tsx
+++ client/src/pages/landing/Salas.tsx
@@ -5,59 +5,12 @@ import axios from "axios";
 export default function Salas() {
   const [salas, setSalas] = useState([]);
 
-  /**
-  const salas = [
-    {
-      id: "1",
-      image: "https://imgur.com/ZLkPmIQ",
-      name: "Sala 1",
-      description: "Descripción de la sala 1",
-      lugar: "Lugar de la sala 1",
-      cupo: "Cupo de la sala 1",
-    },
-    {
-      id: "2",
-      image: "https://imgur.com/ZLkPmIQ",
-      name: "Sala 2",
-      description: "Descripción de la sala 2",
-      lugar: "Lugar de la sala 2",
-      cupo: "Cupo de la sala 2",
-    },
-    {
-      id: "3",
-      image: "https://imgur.com/ZLkPmIQ",
-      name: "Sala 3",
-      description: "Descripción de la sala 3",
-      lugar: "Lugar de la sala 3",
-      cupo: "Cupo de la sala 3",
-    },
-  ];
-   */
-
-  /**
-   * Delete once Axios is implemented
-  useEffect(() => {
-    const fetchSalas = async () => {
-      try {
-        const response = await fetch("http://localhost:4000/salas");
-        if (!response.ok) throw new Error("Response not ok");
-
-        const data = await response.json();
-        setSalas(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchSalas();
-  }, []);
-   */
-
   useEffect(() => {
     axios
-      .get("http//:localhost:4000/salas")
+      .get("https://dreamapi.azurewebsites.net/salas")
       .then(function (res) {
-        setSalas([]);
+        console.log(res.data);
+        setSalas(res.data);
       })
       .catch(function (err) {
         console.log(err);
@@ -76,13 +29,13 @@ export default function Salas() {
         {salas &&
           salas.map(
             (sala: {
-              id: string;
-              image: string;
-              name: string;
-              description: string;
-              lugar: string;
-              cupo: string;
-            }) => <SalaCard key={sala.id} sala={sala} />,
+              SalaId: string;
+              Link: string;
+              Nombre: string;
+              Descripcion: string;
+              Lugar: string;
+              Cupo: string;
+            }) => <SalaCard key={sala.SalaId} sala={sala} />,
           )}
       </div>
     </div>
