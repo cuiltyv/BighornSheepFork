e92ed4b020f4fd94d870a8af7881636ee41f3db0
Tellotello <a01721735@tec.mx>
2024-05-16 20:42:42 -0600

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BighornSheep/",
+  base: "/",
   resolve: {
     alias: {
       "@api_helper": path.resolve(__dirname, "./src/api/"),
-      "@UserAuth": path.resolve(__dirname,"./src/context"),
+      "@UserAuth": path.resolve(__dirname, "./src/context"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  /*
   build: {
     outDir: 'dist', 
   }
-})
-
+  */
+});
