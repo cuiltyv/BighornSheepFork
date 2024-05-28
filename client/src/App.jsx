96cefe604339eaf40715d30d0bc76ae51234516b
cuiltyv <88845787+cuiltyv@users.noreply.github.com>
2024-05-27 19:56:51 -0600

    muchos cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Unauthorized from "./pages/extra/Unauthorized";
 import RequireAuth from "./components/RequireAuth";
 import PersistLogin from "./components/PersistLogin";
 import { Routes, Route } from "react-router-dom";
+import useAuth from "@UserAuth";
 
 import WithLayout from "./components/WithLayout";
 import WithLayoutLogout from "./components/WithLayoutLogout";
@@ -18,12 +19,16 @@ import EventManager from "./pages/admin/EventManager";
 import VideoManager from "./pages/admin/VideoManager";
 import UserManagement from "./pages/admin/UserManagement";
 import AdminStats from "./pages/admin/AdminStats";
+import { useEffect } from "react";
 const ROLES = {
   User: 1,
   Admin: 2,
 };
 
 function App() {
+  const { auth } = useAuth();
+  const userID = auth?.userID;
+
   return (
     <div className="bg-darkWhite">
       <Routes>
@@ -32,7 +37,6 @@ function App() {
           <Route path="/BighornSheep/landing" element={<Landing />} />
           <Route path="/BighornSheep/login" element={<Login />} />
           <Route path="/" element={<Landing />} />
-          <Route path="*" element={<Login />} />
         </Route>
         <Route path="/" element={<WithLayoutLogout />}>
           {/* Ruta solo para usuarios autenticados */}
@@ -47,7 +51,6 @@ function App() {
               <Route path="/BighornSheep" element={<Home />} />
               <Route path="/BighornSheep/perfil" element={<Perfil />} />
             </Route>
-
             {/* Ruta solo para admin */}
             <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
               <Route path="/BighornSheep/admin" element={<Dashboard />} />
@@ -65,15 +68,12 @@ function App() {
               />
               <Route path="/BighornSheep/adminstats" element={<AdminStats />} />
             </Route>
-
             <Route
               path="/BighornSheep/unauthorized"
               element={<Unauthorized />}
             />
+            <Route path="*" element={userID ? <Missing /> : <Login />} />
           </Route>
-
-          {/* Ruta si no hay match */}
-          <Route path="*" element={<Missing />} />
         </Route>
       </Routes>
     </div>
